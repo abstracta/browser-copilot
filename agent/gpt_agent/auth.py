e0e62dc7e471558edc39e81b59ad96baeb9b2cab
rabelenda <rabelenda@gmail.com>
2024-03-01 17:05:27 -0300

    Avoid failing login with openid connect when signing keys are updated on provider

--- agent/gpt_agent/auth.py
+++ agent/gpt_agent/auth.py
@@ -1,19 +1,14 @@
+import datetime
 import os
 import traceback
 from typing import Optional, Annotated, Any
-import requests
 
+import requests
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OpenIdConnect
 from fastapi.security.utils import get_authorization_scheme_param
 from jose import JWTError, jwt
 from starlette.requests import Request
-
-
-def _extract_jwks(config_url: str) -> Any:
-    config_resp = requests.get(config_url)
-    ret_resp = requests.get(config_resp.json()['jwks_uri'])
-    return ret_resp.json()
 
 
 def _build_auth_exception() -> HTTPException:
@@ -30,21 +25,52 @@ class BearerOpenIdConnect(OpenIdConnect):
         return param
 
 
+class OpenIdConfig:
+
+    def __init__(self, url: str):
+        self.url = url
+        self._last_update = None
+        self._keys = None
+
+    def get_updated_keys(self, period: datetime.timedelta) -> Any:
+        if self._last_update is None or datetime.datetime.utcnow() - self._last_update > period:
+            self._update_keys()
+        return self._keys
+
+    def _update_keys(self):
+        self.keys_last_update = datetime.datetime.utcnow()
+        config_resp = requests.get(self.url)
+        ret_resp = requests.get(config_resp.json()['jwks_uri'])
+        self._keys = ret_resp.json()
+
+
 openid_url = os.getenv("OPENID_URL")
 if openid_url is not None:
     config_url = openid_url + "/.well-known/openid-configuration"
-    openid_keys = _extract_jwks(config_url)
+    openid_config = OpenIdConfig(config_url)
     auth_scheme = BearerOpenIdConnect(openIdConnectUrl=config_url)
 else:
-    openid_keys = None
+    openid_config = None
     auth_scheme = lambda: None
+
+
+def _decode_token(token: str) -> dict:
+    openid_keys = openid_config.get_updated_keys(datetime.timedelta(days=1))
+    options = {"verify_aud": False}
+    try:
+        return jwt.decode(token, openid_keys, options=options)
+    except JWTError as e:
+        new_keys = openid_config.get_updated_keys(datetime.timedelta(minutes=5))
+        if new_keys == openid_keys:
+            raise e
+        return jwt.decode(token, new_keys, options=options)
 
 
 async def get_current_user(token: Annotated[Optional[str], Depends(auth_scheme)]) -> str:
     if openid_url is None:
         return ""
     try:
-        payload = jwt.decode(token, openid_keys, options={"verify_aud": False})
+        payload = _decode_token(token)
         username = payload.get("email")
         # In Azure Authentication we haven't been able to get email attribute, but it is contained
         # in this attribute
