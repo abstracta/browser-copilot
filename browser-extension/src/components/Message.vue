f50e7e2d0405ead97dfcb362aff08d4bfa80b4ab
rabelenda <rabelenda@gmail.com>
2024-11-19 15:39:28 -0300

    Fix issues with charts not showing when navigating to new url or refresh
    Additionally, update dependencies

--- browser-extension/src/components/Message.vue
+++ browser-extension/src/components/Message.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, nextTick, ref } from 'vue'
+import { computed, nextTick, ref, onMounted, onBeforeUnmount } from 'vue'
 import { useI18n } from 'vue-i18n'
 import MarkdownIt from 'markdown-it'
 import hljs from 'highlight.js'
@@ -13,61 +13,11 @@ import moment from 'moment'
 
 const props = defineProps<{ text: string, file: Record<string, string>, isUser: boolean, isComplete: boolean, isSuccess: boolean, agentLogo: string, agentName: string, agentId: string }>()
 const { t } = useI18n()
+const renderedMsg = computed(() => props.isUser ? props.text.replaceAll('\n', '<br/>') : renderMarkDown(props.text))
 const messageElement = ref<HTMLElement | null>(null);
-
-function useTargetBlankLinks(md: MarkdownIt) {
-  let defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
-    return self.renderToken(tokens, idx, options)
-  }
-  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-    tokens[idx].attrSet('target', '_blank')
-    return defaultRender(tokens, idx, options, env, self)
-  }
-}
-
-function formatEpoch(config:any): (value: any) => string {
-  return (value: any) => {
-    if (typeof value === 'object') {
-      value = value.value;
-    }
-    const time = moment(parseInt(value))
-    return time.format(config.format);
-  }
-}
-
-function solveEchartsFormatter(obj: any) {
-  if (obj && obj.formatter) {
-    if (obj.formatter.name === 'formatEpoch') {
-      obj.formatter = formatEpoch(obj.formatter)
-    }
-  }
-}
-
-function useEcharts(md: MarkdownIt) {
-  const defaultRender = md.renderer.rules.fence || function(tokens, idx, options, env, self) {
-    return self.renderToken(tokens, idx, options);
-  };
-  md.renderer.rules.fence = function(tokens, idx, options, env, self) {
-    const token = tokens[idx];
-    const code = token.content.trim();
-    if (token.info === 'echarts') {
-      nextTick().then(() => {
-        const container = messageElement.value!;
-        const chartDiv = container.querySelector('.echarts');
-        const chartData = container.querySelector('.echarts-data');
-        if (chartDiv && chartData) {
-          const chart = echarts.init(chartDiv as HTMLDivElement);
-          const options = JSON.parse(chartData.textContent || '');
-          solveEchartsFormatter(options.xAxis.axisLabel);
-          solveEchartsFormatter(options.xAxis.axisPointer.label);
-          chart.setOption(options);
-        }
-      });
-      return `<div class="echarts" style="width: 100%; height: 200px;"></div><div class="echarts-data" style='display:none'>${code}</div>`;
-    }
-    return defaultRender(tokens, idx, options, env, self);
-  };
-}
+const resizeObserver: ResizeObserver = new ResizeObserver(onResize)
+var chart: any;
+var prevWidth: number = 0;
 
 function renderMarkDown(text: string) {
   let md = new MarkdownIt({
@@ -87,23 +37,93 @@ function renderMarkDown(text: string) {
   return md.render(text)
 }
 
-const renderedMsg = computed(() => props.isUser ? props.text.replaceAll('\n', '<br/>') : renderMarkDown(props.text))
+function useTargetBlankLinks(md: MarkdownIt) {
+  let defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options)
+  }
+  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    tokens[idx].attrSet('target', '_blank')
+    return defaultRender(tokens, idx, options, env, self)
+  }
+}
 
+function useEcharts(md: MarkdownIt) {
+  const defaultRender = md.renderer.rules.fence || function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+  md.renderer.rules.fence = function (tokens, idx, options, env, self) {
+    const token = tokens[idx];
+    const code = token.content.trim();
+    if (token.info === 'echarts') {
+      nextTick().then(() => {
+        const container = messageElement.value!;
+        const chartDiv = container.querySelector('.echarts');
+        const chartData = container.querySelector('.echarts-data');
+        if (chartDiv && chartData) {
+          chart = echarts.init(chartDiv as HTMLDivElement);
+          const options = JSON.parse(chartData.textContent || '');
+          solveEchartsFormatter(options.xAxis.axisLabel);
+          solveEchartsFormatter(options.xAxis.axisPointer.label);
+          chart.setOption(options);
+        }
+      });
+      return `<div class="echarts" style="width: 100%; height: 200px;"></div><div class="echarts-data" style='display:none'>${code}</div>`;
+    }
+    return defaultRender(tokens, idx, options, env, self);
+  };
+}
+
+function solveEchartsFormatter(obj: any) {
+  if (obj && obj.formatter) {
+    if (obj.formatter.name === 'formatEpoch') {
+      obj.formatter = formatEpoch(obj.formatter)
+    }
+  }
+}
+
+function formatEpoch(config: any): (value: any) => string {
+  return (value: any) => {
+    if (typeof value === 'object') {
+      value = value.value;
+    }
+    const time = moment(parseInt(value))
+    return time.format(config.format);
+  }
+}
+
+function onResize() {
+  if (messageElement.value!.scrollWidth != prevWidth && chart) {
+    prevWidth = messageElement.value!.scrollWidth;
+    chart.resize();
+  }
+}
+
+onMounted(() => {
+  if (messageElement.value) {
+    resizeObserver.observe(messageElement.value)
+  }
+})
+
+onBeforeUnmount(() => {
+  if (messageElement.value) {
+    resizeObserver.unobserve(messageElement.value)
+  }
+})
 </script>
 
 <template>
-  <div class="flex flex-col mb-1 p-1 min-w-7" :class=" !isSuccess ? ['border-red-500', 'border-b']: []">
+  <div class="flex flex-col mb-1 p-1 min-w-7" :class="!isSuccess ? ['border-red-500', 'border-b'] : []">
     <div class="flex items-center flex-row">
       <template v-if="isUser">
-        <circle-filled-icon class="text-violet-600"/>
+        <circle-filled-icon class="text-violet-600" />
       </template>
       <template v-else-if="!isUser && isSuccess">
         <img :src="agentLogo" class="w-5 mr-1 rounded-full" />
       </template>
       <template v-else>
-        <exclamation-circle-icon class="text-red-600"/>
+        <exclamation-circle-icon class="text-red-600" />
       </template>
-      
+
       <span class="text-base">{{ isUser ? t('you') : agentName }}</span>
       <div class="flex-auto flex justify-end">
         <CopyButton v-if="!isUser && text" :text="text" :html="renderedMsg" />
@@ -118,7 +138,8 @@ const renderedMsg = computed(() => props.isUser ? props.text.replaceAll('\n', '<
           </audio>
         </template>
         <template v-if="text">
-          <div v-html="renderedMsg" ref="messageElement" class="flex flex-col text-sm font-light leading-tight gap-4 rendered-msg"/>
+          <div v-html="renderedMsg" ref="messageElement"
+            class="flex flex-col text-sm font-light leading-tight gap-4 rendered-msg" />
         </template>
       </div>
       <div class="ml-3 dot-pulse" v-if="!isComplete" />
@@ -181,7 +202,7 @@ div a {
   padding: var(--half-spacing);
 }
 
-.rendered-msg > img  {
+.rendered-msg>img {
   box-shadow: var(--shadow);
   border-radius: var(--spacing);
   width: fit-content;
